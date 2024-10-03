b64e8feb07728775024e93b0aff95a641d0e6671
Leonardo Segala <lallo.segala@gmail.com>
2024-10-03 16:23:53 +0200

    Add function docs & function logging

--- alumix/parser.py
+++ alumix/parser.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 
 import bs4
 from dataclasses import dataclass
+import logging
+
+logger = logging.getLogger("alumix")
 
 @dataclass
 class MenuChoice:
@@ -25,6 +28,7 @@ class MenuChoice:
                 "ingredients": self.ingredients}
 
 def find_deepest_node(tag: bs4.Tag, *, cur_depth: int = 0) -> tuple[bs4.Tag, int]:
+    """find the deepest children node of a tag"""
 
     result = [find_deepest_node(child, cur_depth=cur_depth + 1) for child in tag if isinstance(child, bs4.Tag)]
 
@@ -34,9 +38,11 @@ def find_deepest_node(tag: bs4.Tag, *, cur_depth: int = 0) -> tuple[bs4.Tag, int
     return max(result, key=lambda t: t[1])
 
 def parse_menu_title(div: bs4.Tag) -> str:
+    """parse a div containing the menu title"""
     return str(div.find("strong").contents[0])
 
 def parse_menu_plate(div: bs4.Tag) -> MenuChoice:
+    """parse a div containing a menu plate"""
 
     return MenuChoice(name=str(div.find("h3").contents[0]),
                       price=float(str(div.find("strong").contents[0]).replace(",", ".")),
@@ -44,6 +50,7 @@ def parse_menu_plate(div: bs4.Tag) -> MenuChoice:
                       ingredients=None if (h5 := div.find("h5")) is None else str(h5.contents[0]))
 
 def parse_menu_div(div: bs4.Tag) -> tuple[str, MenuChoice]:
+    """parse a div containing a title or a menu plate"""
 
     if "sfCat" in div.attrs.get("class", []):
         return parse_menu_title(div), MenuChoice.empty()
@@ -52,6 +59,7 @@ def parse_menu_div(div: bs4.Tag) -> tuple[str, MenuChoice]:
         return "", parse_menu_plate(div)
 
 def parse_table_row(tr: bs4.Tag) -> tuple[str, str]:
+    """parse a tr tag, returning the table values"""
 
     key, value = tr.find_all("td")
 
@@ -59,6 +67,7 @@ def parse_table_row(tr: bs4.Tag) -> tuple[str, str]:
             str(find_deepest_node(value)[0].contents[0]))
 
 def parse_ul(ul: bs4.Tag) -> dict[str, list[MenuChoice]]:
+    """parse a entire menu returning a dict containing for each category the title and plates"""
 
     fields: dict[str, list[MenuChoice]] = {}
     last_title = None
@@ -85,6 +94,10 @@ def parse_ul(ul: bs4.Tag) -> dict[str, list[MenuChoice]]:
     return fields
 
 def parse_menu(html: str | bytes) -> dict:
+    """parse the menu and extra data in a html file"""
+
+    logger.debug("Parsing html data")
+
     soup = bs4.BeautifulSoup(html, "html.parser")
 
     result: dict[str, list] = {}
